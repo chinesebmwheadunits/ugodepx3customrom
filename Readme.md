129f93cb2685e21cbe60d84c296a4fef1d1fa246
Jan-Willem Spuij <jws@birdseyeview.nl>
2019-04-24 16:44:26 +0200

    script

--- Readme.md
+++ Readme.md
@@ -1,6 +1,8 @@
 # Build script and sources for UGode PX3 Custom rom.
 
-To build a custom ROM run ./script.sh from a linux (ubuntu) bash shell.
+To build a custom ROM run ./makerom.sh from a linux (ubuntu) bash shell. Suggested is to place the px3 unpatched rom into an img/ folder and run
+
+./makerom.sh -f img/px3.img
 
 ## Requirements
 
--- makerom.sh
+++ makerom.sh
@@ -20,6 +20,37 @@ installtools()
 	chmod a+x tools/imgrepackerrk
 }
 
+clean()
+{
+	rm -rf build/
+}
+
+unpack()
+{
+	./tools/imgrepackerrk $filename
+	mv "$filename.dump" ./build/px3.img.dump/
+}
+
+expandsystemimage()
+{
+	dd if=/dev/zero bs=1M count=64 >> ./build/px3.img.dump/Image/system.img
+	resize2fs ./build/px3.img.dump/Image/system.img
+}
+
+mountsystemimage()
+{
+	mkdir ./build/mnt
+	mount ./build/px3.img.dump/Image/system.img ./build/mnt
+}
+
+unmountsystemimage()
+{
+	if mount | grep $(readlink -f ./build/mnt) > /dev/null; then
+		umount ./build/mnt
+		rmdir ./build/mnt
+	fi
+}
+
 while [ "$1" != "" ]; do
     case $1 in
         -f | --file )           shift
@@ -39,7 +70,22 @@ if [ ! -f "$filename" ]; then
 	exit 1
 fi
 
+if [[ $(id -u) -ne 0 ]]; then
+	usage
+	echo "Please run this script as root"
+	exit 1
+fi
+
 if [ ! -d "tools" ]; then
 	installtools
 fi
 
+if [ -d "build" ]; then
+	clean
+fi
+
+mkdir build
+unpack
+expandsystemimage
+mountsystemimage
+unmountsystemimage
